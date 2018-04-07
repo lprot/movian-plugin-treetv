84046940183ccc773b301d1b7cab31f841309934
lprot <lprot@list.ru>
2018-04-07 10:56:48 +0300

    ver bump:   plugin.json 	updated UA:   treetv.js

--- treetv.js
+++ treetv.js
@@ -53,10 +53,10 @@ settings.globalSettings(plugin.id, plugin.title, logo, plugin.synopsis);
 settings.createBool('debug', 'Enable debug logging',  false, function(v) {
     service.debug = v;
 }); 
-settings.createString('UA', 'User-Agent',  'Mozilla/5.0 (Windows NT 6.1; rv:54.0) Gecko/20100101 Firefox/54.0', function(v) {
+settings.createString('UA', 'User-Agent',  'Mozilla/5.0 (Windows NT 6.1; rv:59.0) Gecko/20100101 Firefox/59.0', function(v) {
     service.UA = v;
 });
-settings.createString('platform', 'Platform',  'Win32', function(v) {
+settings.createString('platform', 'Platform',  'Win64', function(v) {
     service.platform = v;
 });
 
@@ -800,6 +800,7 @@ function getDoc(url) {
         });
         isUAset++;
     }
+print(unescape(url));
     return http.request(unescape(url)).toString();        
 }
 
